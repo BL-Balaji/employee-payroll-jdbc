30e884b0cc55b11bb3de22f7bba9241b39f88571
BL-Balaji <balaji.sapkal@bridgelabz.com>
2026-03-04 14:51:44 +0530

    [Balaji]: UC3 – Insert Employee Payroll Data.

---
+++ sql/UC3 – Insert Employee Payroll Data.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee_payroll (name, salary, start)
+VALUES
+('Bill', 100000, '2018-01-01'),
+('Charlie', 120000, '2019-02-10'),
+('Terisa', 150000, '2020-03-15');
+
+SELECT * FROM employee_payroll;
