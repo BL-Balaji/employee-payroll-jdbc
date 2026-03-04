466d66aea0ab176c412e501221d78196cdad2a85
BL-Balaji <balaji.sapkal@bridgelabz.com>
2026-03-04 19:05:15 +0530

    [Balaji]: Retrieve employee payroll data from the employee_payroll table using JDBC and display it.

--- employee-payroll-jdbc/src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ employee-payroll-jdbc/src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,19 +1,42 @@
 package com.bridgelabz;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class EmployeePayrollService {
 
+    public void readEmployeePayrollData() {
+
+        try {
+
+            Connection connection = DBConnection.getConnection();
+
+            String query = "SELECT * FROM employee_payroll";
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double salary = resultSet.getDouble("salary");
+                String startDate = resultSet.getString("start");
+
+                System.out.println(id + " | " + name + " | " + salary + " | " + startDate);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
 
-        Connection connection = DBConnection.getConnection();
+        EmployeePayrollService service = new EmployeePayrollService();
 
-        if (connection != null) {
-
-            System.out.println("Connection Established Successfully");
-
-        } else {
-
-            System.out.println("Connection Failed");
-        }
+        service.readEmployeePayrollData();
     }
 }
