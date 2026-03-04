9b6b8fb49c8c18bdae4d588c941fad06a0606970
BL-Balaji <balaji.sapkal@bridgelabz.com>
2026-03-04 16:05:31 +0530

    [Balaji]: UC10 – Insert Terisa in Sales and Marketing.

---
+++ sql/UC10 – Insert Terisa in Sales and Marketing.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee_payroll
+(name, salary, start, gender, department)
+VALUES
+('Terisa', 200000, '2020-04-10', 'F', 'Sales'),
+('Terisa', 200000, '2020-04-10', 'F', 'Marketing');
+
+SELECT * FROM employee_payroll;
